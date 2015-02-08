1c6e245158f998b278f0d2445b5774f2a3b7f12b
Chris Chen <ChristopherChen2018@u.northwestern.edu>
2015-02-08 14:10:02 -0600

    Added a stopwatch

--- WildHunt/app/src/main/java/com/ieee_ae/wildhunt/MapsActivity.java
+++ WildHunt/app/src/main/java/com/ieee_ae/wildhunt/MapsActivity.java
@@ -1,7 +1,12 @@
 package com.ieee_ae.wildhunt;
 
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.os.Handler;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -20,11 +25,41 @@ public class MapsActivity extends FragmentActivity implements MeteorCallback {
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
     private Meteor mMeteor; // Server connection
 
+    // Stopwatch Stuff
+    private TextView textTimer;
+    private Button startButton;
+    private Button pauseButton;
+    private long startTime = 0L;
+    private Handler myHandler = new Handler();
+    long timeInMillies = 0L;
+    long timeSwap = 0L;
+    long finalTime = 0L;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         setUpMapIfNeeded();
+
+        // Stopwatch stuff
+        textTimer = (TextView) findViewById(R.id.stopwatch);
+
+        startButton = (Button) findViewById(R.id.btnStart);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                startTime = SystemClock.uptimeMillis();
+                myHandler.postDelayed(updateTimerMethod, 0);
+
+            }
+        });
+        pauseButton = (Button) findViewById(R.id.btnPause);
+        pauseButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                timeSwap += timeInMillies;
+                myHandler.removeCallbacks(updateTimerMethod);
+
+            }
+        });
 
         mMeteor = new Meteor("ws://wildhunt.meteor.com/websocket");
         mMeteor.setCallback(this);
@@ -79,6 +114,24 @@ public class MapsActivity extends FragmentActivity implements MeteorCallback {
         mMap.addMarker(new MarkerOptions().position(rogersHouse).title("Rogers House")
                 .snippet("Chris Chen lives here!"));
     }
+
+    private Runnable updateTimerMethod = new Runnable() {
+
+        public void run() {
+            timeInMillies = SystemClock.uptimeMillis() - startTime;
+            finalTime = timeSwap + timeInMillies;
+
+            int seconds = (int) (finalTime / 1000);
+            int minutes = seconds / 60;
+            seconds = seconds % 60;
+            int milliseconds = (int) (finalTime % 1000);
+            textTimer.setText("" + minutes + ":"
+            + String.format("%02d", seconds) + ":"
+            + String.format("%03d", milliseconds));
+            myHandler.postDelayed(this, 0);
+        }
+
+    };
 
 
     /*
